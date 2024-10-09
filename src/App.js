c6b765fec7d9f17dbcc478c434dd2fd24409a6e8
AnkushM11 <ankushmehta1106@gmail.com>
2024-10-09 22:09:26 +0530

    Deepconvo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      {/* Chatbot iframe */}
+      <div className="chatbot-container open">
+        <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+        <df-messenger
+          chat-title="DeepConvo"
+          agent-id="ebeea2f7-72e7-4ca3-b0f4-8746e5c8e0f6"
+          language-code="en"
+          df-messenger-chat-border-radius="10"
+          chat-width="220"
+          chat-height="380"
+        ></df-messenger>
+      </div>
     </div>
   );
 }
